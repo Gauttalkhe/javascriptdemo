eab6df7a98e46d17acd947c9aea957242fec8de7
gautami <gautamitalkhe1234@gmail.com>
2024-07-08 23:09:19 +0530

    conversionoperation

---
+++ 04_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 33
+let score1 ="30xyz"
+let score2 = null
+console.log(typeof score)
+console.log(typeof(score1));
+console.log(typeof score2);
+
+let valueInNumber = Number(score2)  // convert string into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+console.log(score2);
+
+// CONVERSION TYEP AND ANS
+// "33" => 33
+//"33ABD" => nan
+// TRUE => 1 ; FALSE => 0
+
+let boolencon = 1
+
+let booleanboolencon = Boolean(boolencon)
+console.log(boolencon);
+
+// BOOLEAN CONVERSION
+// 1 => true; 0 => false
+//"" => false
+// "gautami" =>true
